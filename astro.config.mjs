73d64e06403d4f6a0043a905d9f307a589757c8c
danny1998cuba <danny.glezcuet98@gmail.com>
2026-02-18 21:20:10 -0500

    repo: initial modifications

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,16 @@
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
+import { SITE_URL } from "./src/consts"
+
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: SITE_URL,
+  integrations: [mdx(), sitemap()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
