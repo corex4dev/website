086075f185b8d9d761b2ca52ce52c0ca3018326a
danny1998cuba <danny.glezcuet98@gmail.com>
2026-02-24 18:21:13 -0500

    feat: theme change base support

---
+++ src/components/theme/index.ts
@@ -0,0 +1,5 @@
+import ThemeManager from "./ThemeManager.astro"
+import ThemeSelect from "./ThemeSelect.astro"
+import ThemeToggleButton from "./ThemeToggleButton.astro"
+
+export { ThemeManager, ThemeSelect, ThemeToggleButton }
