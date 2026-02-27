0f40a67614d39964862ebe7c7b5f709ec18d6ba0
danny1998cuba <danny.glezcuet98@gmail.com>
2026-02-26 20:21:29 -0500

    feat: cookie preferences

--- src/globals.d.ts
+++ src/globals.d.ts
@@ -1,4 +1,8 @@
 interface Window {
+  __ENZUZO_STARTED__?: boolean;
+  __enzuzoApi?: {
+    prefCenter: { show: () => void };
+  };
   theme: {
     setTheme: (theme: "auto" | "dark" | "light") => void;
     getTheme: () => "auto" | "dark" | "light";
